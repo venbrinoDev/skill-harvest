ec3d7fb2ac397d5d4db9007dbfa419fee1374527
venbrinoDev <venbrinosoftwaredeveloper@gmail.com>
2024-02-22 09:52:49 +0100

    worked on do-while-loop

--- lib/controlFlow/while.dart
+++ lib/controlFlow/while.dart
@@ -1,16 +1,35 @@
-void main() {
+void main() async {
   //in the while we write condition inside the loop
   int i = 0;
 
-  while (i < 30) {
-    // print("Befroe adding  $i\n");
+  // while (i < 20) {
+  //   print("Update server......");
 
+  //   await Future.delayed(Duration(milliseconds: 2000));
+
+  //   ///Server broke done
+  //   if (i == 10) {
+  //     print(" server Broke down at $i");
+  //     break;
+  //   }
+
+  //   i = i + 2;
+  // }
+
+  while (i < 20) {
     i = i + 2;
+    
+    print("Update server......");
+
+    await Future.delayed(Duration(milliseconds: 1000));
+
+    ///Server broke done
+    if (i == 10) {
+      print(" server Broke down at But Continue");
+      continue;
+    }
+    print("Update done  in $i");
   }
 
-  while (i > 30) {
-    print("Update server");
-
-    i = i + 2;
-  }
+  print(" you are out of while loop");
 }
